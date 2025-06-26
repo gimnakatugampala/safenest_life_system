c5004417840ede6ec9ad40ef924bd1aab9166660
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-06-26 14:36:47 +0530

    footer

--- auth/register.php
+++ auth/register.php
@@ -224,14 +224,5 @@
 			</div>
 		</div>
 	</div>
-	<!-- success Popup html End -->
-	<!-- js -->
-	<script src="../vendors/scripts/core.js"></script>
-	<script src="../vendors/scripts/script.min.js"></script>
-	<script src="../vendors/scripts/process.js"></script>
-	<script src="../vendors/scripts/layout-settings.js"></script>
-	<script src="../src/plugins/jquery-steps/jquery.steps.js"></script>
-	<script src="../vendors/scripts/steps-setting.js"></script>
-</body>
 
-</html>
+	<?php include_once '../includes/footer.php'; ?>
