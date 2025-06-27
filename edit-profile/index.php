f78849ba990128a62a16b0f489ccbdeee9e9af74
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-06-27 22:28:13 +0530

    edit-profile

--- edit-profile/index.php
+++ edit-profile/index.php
@@ -27,12 +27,12 @@
 					<div class="row">
 						<div class="col-md-12 col-sm-12">
 							<div class="title">
-								<h4>Profile</h4>
+								<h4>Edit Profile</h4>
 							</div>
 							<nav aria-label="breadcrumb" role="navigation">
 								<ol class="breadcrumb">
 									<li class="breadcrumb-item"><a href="index.html">Home</a></li>
-									<li class="breadcrumb-item active" aria-current="page">Profile</li>
+									<li class="breadcrumb-item active" aria-current="page">Edit Profile</li>
 								</ol>
 							</nav>
 						</div>
@@ -44,7 +44,7 @@
 					<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 mb-30">
 						<div class="pd-20 card-box height-100-p">
 							<div class="profile-photo">
-								<a href="../edit-profile/" class="edit-avatar"><i class="fa fa-pencil"></i></a>
+								<a href="modal" data-toggle="modal" data-target="#modal" class="edit-avatar"><i class="fa fa-pencil"></i></a>
 								<img src="../vendors/images/photo1.jpg" alt="" class="avatar-photo">
 								<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
 									<div class="modal-dialog modal-dialog-centered" role="document">
@@ -86,8 +86,40 @@
 									</li>
 								</ul>
 							</div>
-							
-							
+							<div class="profile-social">
+								<h5 class="mb-20 h5 text-blue">Social Links</h5>
+								<ul class="clearfix">
+									<li><a href="#" class="btn" data-bgcolor="#3b5998" data-color="#ffffff"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#1da1f2" data-color="#ffffff"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#007bb5" data-color="#ffffff"><i class="fa fa-linkedin"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#f46f30" data-color="#ffffff"><i class="fa fa-instagram"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#c32361" data-color="#ffffff"><i class="fa fa-dribbble"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#3d464d" data-color="#ffffff"><i class="fa fa-dropbox"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#db4437" data-color="#ffffff"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#bd081c" data-color="#ffffff"><i class="fa fa-pinterest-p"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#00aff0" data-color="#ffffff"><i class="fa fa-skype"></i></a></li>
+									<li><a href="#" class="btn" data-bgcolor="#00b489" data-color="#ffffff"><i class="fa fa-vine"></i></a></li>
+								</ul>
+							</div>
+							<div class="profile-skills">
+								<h5 class="mb-20 h5 text-blue">Key Skills</h5>
+								<h6 class="mb-5 font-14">HTML</h6>
+								<div class="progress mb-20" style="height: 6px;">
+									<div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<h6 class="mb-5 font-14">Css</h6>
+								<div class="progress mb-20" style="height: 6px;">
+									<div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<h6 class="mb-5 font-14">jQuery</h6>
+								<div class="progress mb-20" style="height: 6px;">
+									<div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<h6 class="mb-5 font-14">Bootstrap</h6>
+								<div class="progress mb-20" style="height: 6px;">
+									<div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+							</div>
 						</div>
 					</div>
 
