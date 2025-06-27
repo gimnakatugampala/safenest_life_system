bfa3fefcec915e550b1ef720ed6bf709d684311c
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-06-27 12:43:08 +0530

    dashboard_page

--- includes/sub_header.php
+++ includes/sub_header.php
@@ -58,7 +58,7 @@
 							<ul>
 								<li>
 									<a href="#">
-										<img src="../vendors/images/img.jpg" alt="">
+										<img src="./vendors/images/img.jpg" alt="">
 										<h3>John Doe</h3>
 										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
 									</a>
@@ -72,28 +72,28 @@
 								</li>
 								<li>
 									<a href="#">
-										<img src="../vendors/images/photo2.jpg" alt="">
+										<img src="./vendors/images/photo2.jpg" alt="">
 										<h3>Erik L. Richards</h3>
 										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
 									</a>
 								</li>
 								<li>
 									<a href="#">
-										<img src="../vendors/images/photo3.jpg" alt="">
+										<img src="./vendors/images/photo3.jpg" alt="">
 										<h3>John Doe</h3>
 										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
 									</a>
 								</li>
 								<li>
 									<a href="#">
-										<img src="../vendors/images/photo4.jpg" alt="">
+										<img src="./vendors/images/photo4.jpg" alt="">
 										<h3>Renee I. Hansen</h3>
 										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
 									</a>
 								</li>
 								<li>
 									<a href="#">
-										<img src="../vendors/images/img.jpg" alt="">
+										<img src="./vendors/images/img.jpg" alt="">
 										<h3>Vicki M. Coleman</h3>
 										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
 									</a>
@@ -107,7 +107,7 @@
 				<div class="dropdown">
 					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
 						<span class="user-icon">
-							<img src="../vendors/images/photo1.jpg" alt="">
+							<img src="./vendors/images/photo1.jpg" alt="">
 						</span>
 						<span class="user-name">Ross C. Lopez</span>
 					</a>
@@ -120,7 +120,7 @@
 				</div>
 			</div>
 			<div class="github-link">
-				<a href="https://github.com/dropways/deskapp" target="_blank"><img src="../vendors/images/github.svg" alt=""></a>
+				<a href="https://github.com/dropways/deskapp" target="_blank"><img src="./vendors/images/github.svg" alt=""></a>
 			</div>
 		</div>
 	</div>
