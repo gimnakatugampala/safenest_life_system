1b4b5e89125a9d0cf50275557b8abc7c19fb16a5
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-06-30 21:53:50 +0530

    pending-policy

--- policies/pending-policy-requests.php
+++ policies/pending-policy-requests.php
@@ -65,7 +65,7 @@
 									<td>29-03-2018</td>
 									<td><span class="badge badge-pill badge-info">Pending</span></td>
 									<td>
-                                        <button type="button" class="btn btn-primary btn-sm"><span class="icon-copy ti-eye"></span></button>
+                                        <button data-toggle="modal" data-target="#bd-example-modal-lg" type="button" class="btn btn-primary btn-sm"><span class="icon-copy ti-eye"></span></button>
                                         <button id="sa-params" type="button" class="btn btn-success btn-sm"><span class="icon-copy ti-check"></span></button>
                                         <button id="sa-warning" type="button" class="btn btn-danger btn-sm"><span class="icon-copy ti-close"></span></button>
                                     </td>
@@ -80,6 +80,37 @@
 			<div class="footer-wrap pd-20 mb-20 card-box">
 				
 			</div>
+
+            <!-- Large Modal -->
+             <div class="modal fade bs-example-modal-lg" id="bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+								<div class="modal-dialog modal-lg modal-dialog-centered">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h4 class="modal-title" id="myLargeModalLabel">Large modal</h4>
+											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										</div>
+										<div class="modal-body">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+											proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+											proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											<button type="button" class="btn btn-primary">Save changes</button>
+										</div>
+									</div>
+								</div>
+							</div>
+
 		</div>
 	</div>
 
