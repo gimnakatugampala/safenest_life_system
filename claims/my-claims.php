9f60ef4040ebc4dcb27c33b43b68c08cd2167e69
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-07-01 18:28:30 +0530

    my-claims

---
+++ claims/my-claims.php
@@ -0,0 +1,117 @@
+<?php include_once '../includes/header.php'; ?>
+
+<body>
+	<div class="pre-loader">
+		<div class="pre-loader-box">
+			<div class="loader-logo"><img src="../vendors/images/deskapp-logo.svg" alt=""></div>
+			<div class='loader-progress' id="progress_div">
+				<div class='bar' id='bar1'></div>
+			</div>
+			<div class='percent' id='percent1'>0%</div>
+			<div class="loading-text">
+				Loading...
+			</div>
+		</div>
+	</div>
+
+	<?php include_once '../includes/sub_header.php'; ?>
+	<?php include_once '../includes/sidebar.php'; ?>
+
+	<div class="main-container">
+		<div class="pd-ltr-20 xs-pd-20-10">
+			<div class="min-height-200px">
+				<div class="page-header">
+					<div class="row">
+						<div class="col-md-12 col-sm-12">
+							<div class="title">
+								<h4>My Claims</h4>
+							</div>
+							<nav aria-label="breadcrumb" role="navigation">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.html">Manage Claims</a></li>
+									<li class="breadcrumb-item active" aria-current="page">My Claims</li>
+								</ol>
+							</nav>
+						</div>
+					</div>
+				</div>
+
+
+				<!-- Export Datatable start -->
+				<div class="card-box mb-30">
+					<div class="pd-20">
+					</div>
+					<div class="pb-20">
+						<table class="table hover multiple-select-row data-table-export nowrap">
+							<thead>
+								<tr>
+                                    <th class="table-plus datatable-nosort"># Claim Code</th>
+									<th class="table-plus datatable-nosort">Amount</th>
+									<th class="table-plus datatable-nosort">Claimed By</th>
+									<th class="table-plus datatable-nosort">Nominee</th>
+									<th class="table-plus datatable-nosort">Requested Date</th>
+									<th>Comment</th>
+									<th>Status</th>
+									<th>Policy</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="table-plus">345345</td>
+									<td>30,000</td>
+									<td>Gimna</td>
+									<td>Kavishka</td>
+									<td>2025-08-08</td>
+									<td>Lorem ipsum ..</td>
+									<td><span class="badge badge-pill badge-info">Pending</span></td>
+									<td>Beginner</td>
+									<td>
+                                         <button data-toggle="modal" data-target="#bd-example-modal-lg"  type="button" class="btn btn-primary btn-sm"><i class="icon-copy fa fa-eye" aria-hidden="true"></i></button>
+                                    </td>
+								</tr>
+							
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<!-- Export Datatable End -->
+			</div>
+			<div class="footer-wrap pd-20 mb-20 card-box">
+				
+			</div>
+
+            <!-- Large Modal -->
+             <div class="modal fade bs-example-modal-lg" id="bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+								<div class="modal-dialog modal-lg modal-dialog-centered">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h4 class="modal-title" id="myLargeModalLabel">Large modal</h4>
+											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										</div>
+										<div class="modal-body">
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+											proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+											proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											<button type="button" class="btn btn-primary">Save changes</button>
+										</div>
+									</div>
+								</div>
+							</div>
+
+		</div>
+	</div>
+
+	<?php include_once '../includes/footer.php'; ?>
